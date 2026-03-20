c07dce06fa8422f737f426bfecee23b8e4a7154b
leeal <leealvin676@gmail.com>
2026-03-20 16:22:13 +0800

    design admindashboard and add admin login file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screen/admin/admin_dashboard.dart';
+import 'screen/admin/adminlogin.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AdminDashBoard(),
+      home: AdminLogin(),
     );
   }
 }
--- lib/screen/admin/adminlogin.dart
+++ lib/screen/admin/adminlogin.dart
@@ -1,13 +1,43 @@
+
 import 'package:flutter/material.dart';
 
-class Adminlogin extends StatelessWidget{
+
+class AdminLogin extends StatelessWidget{
   const AdminLogin({super.key});
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Container(
+          height: 600,
+          width:350,
+          decoration: BoxDecoration(
+            color: Color(0xFF333333),
+            borderRadius: BorderRadius.circular(20)
+          ),
+          child: Column(
+            children: [
+              Image.asset('assets/icons/login_icon.png',
+               ),
+              Text("Admin Portal",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              Text("Admin Email"),
+            ],
+          ),
+        ),
+      )
     );
   }
+
 }
+
+
+
