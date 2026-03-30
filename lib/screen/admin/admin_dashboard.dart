bbf2fca59582e8f24833c33d269f626051c15e21
leeal <leealvin676@gmail.com>
2026-03-30 16:55:54 +0800

    add container

--- lib/screen/admin/admin_dashboard.dart
+++ lib/screen/admin/admin_dashboard.dart
@@ -1,17 +1,42 @@
 import "package:flutter/material.dart";
+import 'screen/admin/workout/workout_page.dart';
 
-class AdminDashBoard extends StatelessWidget {
+class AdminDashBoard extends StatefulWidget {
   const AdminDashBoard({super.key});
+
+  @override
+  State<AdminDashBoard> createState() => _AdminDashBoardState();
+}
+
+class _AdminDashBoardState extends State<AdminDashBoard> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
       backgroundColor: Colors.black,
-      body: _mainBody(),
-      bottomNavigationBar: _bottom(),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: _bottom(_currentIndex, (index) {
+        setState(() {
+          _currentIndex = index;
+        });
+      }),
     );
   }
+
+  List<Widget> _pages = [
+    _mainBody(),
+    Center(
+      child: Text('Worko', style: TextStyle(color: Colors.white)),
+    ),
+    Center(
+      child: Text('Trainer Page', style: TextStyle(color: Colors.white)),
+    ),
+    Center(
+      child: Text('User Page', style: TextStyle(color: Colors.white)),
+    ),
+  ];
 }
 
 AppBar _buildAppBar() {
@@ -70,18 +95,63 @@ Widget _mainBody() {
               Text(
                 'Total Users',
                 style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  color: Colors.white
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
               ),
               Spacer(),
-              
-              Row(
-                children: [
-                  Icon(Icons.people),
-                  Text('120')
-                ],
-              )
+
+              Row(children: [Icon(Icons.people), Text('120')]),
+            ],
+          ),
+        ),
+        SizedBox(height: 30),
+        Container(
+          height: 150,
+          width: 350,
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Color(0xFF444444),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Total Users',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Spacer(),
+
+              Row(children: [Icon(Icons.people), Text('120')]),
+            ],
+          ),
+        ),
+        SizedBox(height: 30),
+        Container(
+          height: 150,
+          width: 350,
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Color(0xFF444444),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Total Users',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Spacer(),
+
+              Row(children: [Icon(Icons.people), Text('120')]),
             ],
           ),
         ),
@@ -90,12 +160,15 @@ Widget _mainBody() {
   );
 }
 
-BottomNavigationBar _bottom() {
+BottomNavigationBar _bottom(int currentIndex, Function(int) onTap) {
   return BottomNavigationBar(
     backgroundColor: Colors.black,
     selectedItemColor: Colors.red,
     unselectedItemColor: Colors.grey,
     type: BottomNavigationBarType.fixed,
+
+    currentIndex: currentIndex,
+    onTap: onTap,
 
     items: [
       BottomNavigationBarItem(icon: Icon(Icons.dashboard), label: 'DashBoard'),
@@ -104,7 +177,7 @@ BottomNavigationBar _bottom() {
         label: 'Workout Types',
       ),
       BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Trainers'),
-      BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Trainers'),
+      BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Users'),
     ],
   );
 }
