29f74802c136c48211485c40798978f138c8c405
leeal <leealvin676@gmail.com>
2026-04-01 11:41:04 +0800

    new workout page

--- lib/screen/admin/workout/workout_page.dart
+++ lib/screen/admin/workout/workout_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-
-class WorkoutPage extends StatefulWidget{
+class WorkoutPage extends StatefulWidget {
   const WorkoutPage({super.key});
 
   @override
@@ -9,12 +8,7 @@ class WorkoutPage extends StatefulWidget{
 }
 
 class _WorkoutPageState extends State<WorkoutPage> {
-
-  List<String> workouts = [
-    'cardio',
-    'yoga',
-    'strenght',
-  ];
+  List<String> workouts = ['cardio', 'yoga', 'strenght'];
 
   @override
   Widget build(BuildContext context) {
@@ -22,21 +16,149 @@ class _WorkoutPageState extends State<WorkoutPage> {
       backgroundColor: Colors.black,
 
       body: Padding(
-          padding: EdgeInsets.all(16),
+        padding: EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('WORKOUT TYPE',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),)
+            Text(
+              'WORKOUT TYPE',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: workouts.length,
+                itemBuilder: (context, index) {
+                  return Dismissible(
+                    key: Key(workouts[index]),
+                    direction: DismissDirection.endToStart,
+
+                    onDismissed: (direction) {
+                      setState(() {
+                        workouts.removeAt(index);
+                      });
+                    },
+
+                    background: Container(
+                      color: Colors.red,
+                      alignment: Alignment.centerRight,
+                      padding: EdgeInsets.only(right: 20),
+                      child: Icon(Icons.delete, color: Colors.white),
+                    ),
+
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF444444),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(Icons.fitness_center, color: Colors.red),
+                          SizedBox(width: 10),
+
+                          Expanded(
+                            child: Text(
+                              workouts[index],
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+
+                          IconButton(
+                            icon: Icon(Icons.edit, color: Colors.white),
+                            onPressed: () => _editWorkout(index),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
+      ),
 
-      )
-
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        onPressed: _addWorkout,
+        child: Icon(Icons.add),
+      ),
     );
   }
-}
+
+  void _addWorkout() {
+    TextEditingController controller = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Add Workout'),
+          content: TextField(
+            controller: controller,
+            decoration: InputDecoration(hintText: 'Enter wokout name'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancle'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  workouts.add(controller.text);
+                });
+                Navigator.pop(context);
+              },
+              child: Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _editWorkout(int index) {
+    TextEditingController controller =
+    TextEditingController(text: workouts[index]);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Edit Workout'),
+          content: TextField(controller: controller),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (controller.text.isNotEmpty) {
+                  setState(() {
+                    workouts[index] = controller.text;
+                  });
+                  Navigator.pop(context);
+                }
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+  }
